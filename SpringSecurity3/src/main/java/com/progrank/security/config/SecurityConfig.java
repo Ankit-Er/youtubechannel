9e668e5d3bb207bcfb04b41a6242d7de7d59e76b
ProgRank <33356193+Ankit-Er@users.noreply.github.com>
2020-11-02 11:56:27 +0530

    UserDetailsManager Interface

--- SpringSecurity3/src/main/java/com/progrank/security/config/SecurityConfig.java
+++ SpringSecurity3/src/main/java/com/progrank/security/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	 @Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
+		 http.csrf().disable();
 		http.authorizeRequests().mvcMatchers("/createUser")
 		.permitAll()
 		.anyRequest().authenticated();
